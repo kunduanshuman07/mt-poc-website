d91fbb13c829a489a6ee998e8788c42257bc7d18
Anshuman Kundu <kundu4coding@gmail.com>
2024-10-05 13:28:47 +0530

    Responsive UI with Date & Time Filter

--- ui-poc/src/components/HeaderGrid.jsx
+++ ui-poc/src/components/HeaderGrid.jsx
@@ -5,7 +5,7 @@ import HeaderTextField from "./HeaderTextField"
 
 const HeaderGrid = () => {
   return (
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{padding: "10px"}}>
+    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 4, md: 12 }} sx={{padding: "10px"}}>
       {
         headerTextfieldprops.map((prop, index) => (
           <Grid key={index} size={{ xs: 2, sm: 4, md: 2.4 }}>
